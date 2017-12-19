fc57ef1bdc5a20a8dcd21396c8866af2bc84bce5
liujiawei <13738068613@163.com>
2017-12-19 11:46:48 +0800

    fix code

--- src/main/java/com/dubbo/hystrix/AbstractHystrixConfigBuilder.java
+++ src/main/java/com/dubbo/hystrix/AbstractHystrixConfigBuilder.java
@@ -19,29 +19,40 @@ public abstract class AbstractHystrixConfigBuilder implements ApplicationContext
 
     public HystrixCommandProperties.Setter buildHystrixCommandProperties(String classSimpleName, String methodName) {
         HystrixCommandProperties.Setter setter = HystrixCommandProperties.Setter().withExecutionTimeoutEnabled(false);
+        // circuitBreakerErrorThresholdPercentage 失败率达到多少熔断
         Integer circuitBreakerErrorThresholdPercentage = getProperty(classSimpleName, methodName, "circuitBreakerErrorThresholdPercentage", Integer.class);
         if (circuitBreakerErrorThresholdPercentage != null) {
             setter.withCircuitBreakerErrorThresholdPercentage(circuitBreakerErrorThresholdPercentage);
         }
-        Integer executionTimeoutInMilliseconds = getProperty(classSimpleName, methodName, "executionTimeoutInMilliseconds", Integer.class);
-        if (executionTimeoutInMilliseconds != null) {
-            setter.withExecutionTimeoutInMilliseconds(executionTimeoutInMilliseconds);
-        }
+        // circuitBreakerSleepWindowInMilliseconds 熔断器开启后关闭的间隔时间 放入流量
         Integer circuitBreakerSleepWindowInMilliseconds = getProperty(classSimpleName, methodName, "circuitBreakerSleepWindowInMilliseconds", Integer.class);
         if (circuitBreakerSleepWindowInMilliseconds != null) {
             setter.withCircuitBreakerSleepWindowInMilliseconds(circuitBreakerSleepWindowInMilliseconds);
         }
+        // circuitBreakerRequestVolumeThreshold 10秒钟达到多少次请求开启熔断功能
         Integer circuitBreakerRequestVolumeThreshold = getProperty(classSimpleName, methodName, "circuitBreakerRequestVolumeThreshold", Integer.class);
         if (circuitBreakerRequestVolumeThreshold != null) {
             setter.withCircuitBreakerRequestVolumeThreshold(circuitBreakerRequestVolumeThreshold);
         }
+        // fallbackEnabled 是否启动降级策略
         Boolean fallbackEnabled = getProperty(classSimpleName, methodName, "fallbackEnabled", Boolean.class);
         if (fallbackEnabled != null) {
             setter.withFallbackEnabled(fallbackEnabled);
         }
+        // circuitBreakerEnabled 是否启用熔断策略
         Boolean circuitBreakerEnabled = getProperty(classSimpleName, methodName, "circuitBreakerEnabled", Boolean.class);
         if (circuitBreakerEnabled != null) {
             setter.withCircuitBreakerEnabled(circuitBreakerEnabled);
+        }
+        // circuitBreakerForceOpen 强制熔断
+        Boolean circuitBreakerForceOpen = getProperty(classSimpleName, methodName, "circuitBreakerForceOpen", Boolean.class);
+        if (circuitBreakerForceOpen != null) {
+            setter.withCircuitBreakerForceOpen(circuitBreakerForceOpen);
+        }
+        // circuitBreakerForceClosed 强制关闭熔断
+        Boolean circuitBreakerForceClosed = getProperty(classSimpleName, methodName, "circuitBreakerForceClosed", Boolean.class);
+        if (circuitBreakerForceClosed != null) {
+            setter.withCircuitBreakerForceClosed(circuitBreakerForceClosed);
         }
         return setter;
     }
@@ -53,6 +64,7 @@ public abstract class AbstractHystrixConfigBuilder implements ApplicationContext
      */
     public HystrixThreadPoolProperties.Setter buildHystrixThreadPoolProperties(String classSimpleName, String methodName) {
         HystrixThreadPoolProperties.Setter setter = HystrixThreadPoolProperties.Setter();
+        // coreSize 线程池核心大小
         Integer coreSize = getProperty(classSimpleName, methodName, "coreSize", Integer.class);
         if (coreSize != null) {
             setter.withCoreSize(coreSize);
@@ -61,6 +73,7 @@ public abstract class AbstractHystrixConfigBuilder implements ApplicationContext
         if (keepAliveTimeMinutes != null) {
             setter.withKeepAliveTimeMinutes(keepAliveTimeMinutes);
         }
+        // queueSizeRejectionThreshold 线程池队列达到多少时拒绝请求
         Integer queueSizeRejectionThreshold = getProperty(classSimpleName, methodName, "queueSizeRejectionThreshold", Integer.class);
         if (queueSizeRejectionThreshold != null) {
             setter.withQueueSizeRejectionThreshold(queueSizeRejectionThreshold);
@@ -71,7 +84,7 @@ public abstract class AbstractHystrixConfigBuilder implements ApplicationContext
     public HystrixMethodConfig buildHystrixMethodConfig(String classSimpleName, String methodName) {
         HystrixMethodConfig methodConfig = new HystrixMethodConfig();
         methodConfig.setGroupKey(classSimpleName);
-        methodConfig.setCommandKey(methodName);
+        methodConfig.setCommandKey(String.format("%s-%s", classSimpleName, methodName));
         methodConfig.setFallbackClass(getProperty(classSimpleName, "default", "fallbackClass", String.class));
         return methodConfig;
     }
