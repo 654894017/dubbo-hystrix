5aaef746c50503f179a0763352e66c2203c8cea5
liujiawei <13738068613@163.com>
2017-12-19 10:51:34 +0800

    注释信息

--- src/main/java/com/dubbo/hystrix/HystrixConfigBuilderFactory.java
+++ src/main/java/com/dubbo/hystrix/HystrixConfigBuilderFactory.java
@@ -3,6 +3,10 @@ package com.dubbo.hystrix;
 /**
  * @author liujiawei
  * @create 2017-12-19 上午12:25
+ * <p>
+ * 熔断器生成工厂接口，实现类配合 AbstractHystrixConfigBuilder使用，逻辑参见 DubboHystrixAutoConfiguration
+ * <p>
+ * 重要:工厂实现类beanName 必须为hystrixConfigBuilderFactory
  */
 public interface HystrixConfigBuilderFactory {
     AbstractHystrixConfigBuilder getHystrixConfigBuilder();
